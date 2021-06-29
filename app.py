cd7627e01f45b45206ed0d3f7a9f8ba470127c82
vincent <vincent.huang1116@gmail.com>
2021-06-29 14:52:53 +0800

    update

--- app.py
+++ app.py
@@ -34,7 +34,11 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text=event.message.text)
+    print(message)
+    print(event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+    '''
     if event.message.text == "sticker":
         pass
     elif event.message.text == "image":
@@ -49,7 +53,7 @@ def handle_message(event):
             "text": "You say "+event.message.text
         }
         line_bot_api.reply_message(event.reply_token, message)
-
+    '''
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
