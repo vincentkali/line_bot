2922f8f7e057e2e8f5ec7c0dad3943c17d732a9f
vincent <vincent.huang1116@gmail.com>
2021-06-30 20:45:19 +0800

    update

--- app.py
+++ app.py
@@ -52,14 +52,13 @@ def handle_message(event):
     elif event.message.text == "template":
         pass
     elif event.message.text == "confirm":
-    	pass
-    	'''
+    	    	
     	confirm_template = ConfirmTemplate(text='Do it?', actions=[
             MessageAction(label='Yes', text='Yes!'),
             MessageAction(label='No', text='No!')])
         message = TemplateSendMessage(
             alt_text='Confirm alt text', template=confirm_template)
-        '''
+        
         
     else:
         message = TextSendMessage(text="You say "+event.message.text)
