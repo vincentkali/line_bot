31c25e6a6e16063350e294266ad8a3caffe29a3c
vincent <vincent.huang1116@gmail.com>
2021-07-02 14:32:51 +0800

    update

---
+++ shell/getFriendList.sh
@@ -0,0 +1,8 @@
+
+
+
+#curl -v -X GET https://api.line.me/v2/bot/followers/ids?start={continuationToken} \
+#-H 'Authorization: Bearer {sTcy0mRQtYKW3r3XnlT80MqWC11JpLqQwdaQ6eRkWVGE9AMPhXvtSP1bMHqn66qmhNOMWdBse5+1zUcoftyJ5MP5AkJz16AJ+DzbGl7zyUBw9Nsoai8j0b9PiVv+G0iuPscvE8plDxDuxd15CmnTPAdB04t89/1O/w1cDnyilFU=}'
+
+curl -v -X GET https://api.line.me/v2/bot/followers/ids \
+-H 'Authorization: Bearer {sTcy0mRQtYKW3r3XnlT80MqWC11JpLqQwdaQ6eRkWVGE9AMPhXvtSP1bMHqn66qmhNOMWdBse5+1zUcoftyJ5MP5AkJz16AJ+DzbGl7zyUBw9Nsoai8j0b9PiVv+G0iuPscvE8plDxDuxd15CmnTPAdB04t89/1O/w1cDnyilFU=}'
